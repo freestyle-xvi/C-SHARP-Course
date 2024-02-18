ffda710b5bcb56d24908934b4326aa63be4b0576
freestyle-XVI <90510503+freestyle-xvi@users.noreply.github.com>
2024-02-18 14:26:26 +0200

    Add files via upload

---
+++ #45 Project/project-xlv/project-xlv/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace project_xlv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //generics
+
+            int[] intArray = {1,2,3};
+            double[] doubleArray = {1.1,2.2,3.3};
+            String [] stringArray = {"-1","-2","-3"};
+
+            displayElements(intArray);
+            displayElements(doubleArray);
+            displayElements(stringArray);
+
+            Console.ReadKey();
+        }
+        public static void displayElements<T>(T [] array)
+        {
+            foreach(T item in array)
+            {
+                Console.WriteLine(item + " ");
+            }
+            Console.WriteLine( );
+        }
+        
+    }
+}
---
+++ #46 Project/project-xlvi/project-xlvi/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+namespace project_xlvi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //multithreading
+
+            Thread mainThread = Thread.CurrentThread;
+
+            mainThread.Name = "Main Thread XVI";
+
+           // Console.WriteLine(mainThread.Name);
+
+            Thread thread1 = new Thread(() => countDown("Timer number 1"));
+            Thread thread2 = new Thread(() => countUp("Timer number 2"));
+
+            thread1.Start();
+            thread2.Start();
+
+            Console.WriteLine(mainThread.Name + " is complete");
+
+
+            Console.ReadKey();
+        }
+        public static void countDown(String name)
+        {
+            for(int i = 10; i >= 0; i-- )
+            {
+                Console.WriteLine("Timer number 1 : " + i + " seconds");
+                Thread.Sleep(1000);//1000 milliseconds = 1 second
+            }
+            Console.WriteLine("\nTimer number 1 is complete!");
+        }
+        public static void countUp(String name  )
+        {
+            for (int i = 0; i <= 10; i++    )
+            {
+                Console.WriteLine("Timer number 2 : " + i + " seconds");
+                Thread.Sleep(1000);//1000 milliseconds = 1 second
+            }
+            Console.WriteLine("\nTimer number 2 is complete!");
+        }
+    }
+}

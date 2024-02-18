4fba9b525d92c972e7ed0b14cdf47b72c9e1b5dc
freestyle-XVI <90510503+freestyle-xvi@users.noreply.github.com>
2024-02-18 13:50:04 +0200

    Add files via upload

---
+++ #7 Project/project-vii/project-vii/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace project_vii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //hypotenuse calculator program
+
+            Console.WriteLine("Enter value of side A of triangle:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter value of side B of triangle:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            double c = Math.Sqrt( (a * a) + (b * b) );
+
+            Console.WriteLine("The hypotenuse of the triangle is: " + c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ #8 Project/project-viii/project-viii/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace project_viii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string methods 
+
+            String fullName = "freestyle XVI";
+            String phoneNumber = "021-069-0420";
+
+            //fullName = fullName.ToUpper();      //changes all characters in string to uppercase
+            //fullName = fullName.ToLower();      //changes all characters in string to lowercase
+            //Console.WriteLine(fullName);
+
+            //phoneNumber =  phoneNumber.Replace("-","/");         //replaces all "-" with "/"
+            //Console.WriteLine(phoneNumber);
+
+            //string userName = fullName.Insert(0, "@");  //inserts an @ sign at the star of the fullname     
+            //Console.WriteLine(userName);
+
+            //Console.WriteLine(fullName.Length); //displays the lenght of the string
+
+            string firstName = fullName.Substring(0,1);//copies certain letters out of a string using the index
+            string lastName = fullName.Substring(10,3);
+            Console.WriteLine(firstName + "-" + lastName);
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}

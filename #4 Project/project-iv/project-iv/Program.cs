8289dc83c7a0a5b72c58ebbe29004a3b17334ca8
freestyle-XVI <90510503+freestyle-xvi@users.noreply.github.com>
2024-02-18 13:46:34 +0200

    Add files via upload

---
+++ #4 Project/project-iv/project-iv/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace project_iv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int friends = 10;
+            // friends = friends + 2;
+            // friends += 2;//adds 2 to the original number
+            //friends++;
+
+            //friends = friends - 1; //subtracts 1 from og value
+            //friends -= 1;
+            // friends--;
+
+
+            //friends = friends * 2;//multiplies og value by 2
+            //  friends *= 2;
+
+            //friends = friends / 2; //divides og value by 2 , since value is integer the reaminder won't be displayed
+            //friends /= 2;
+
+            int remainder = friends % 3; // shows only the remainder after dividing
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine(friends);
+            Console.WriteLine(remainder);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ #6 Project/project-vi/project-vi/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace project_vi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            //int num = random.Next(1, 7);                  //will generate a random number in the range of 1-6 
+            //int num = random.Next(1, 7) + 100;    //range is now from 101-106
+            //int num = random.Next(1, 21);             //range from 1 -21
+            // double num = random.NextDouble();   //random number will be from 0-1(integer value)
+            int num1 = random.Next(1, 7);
+            int num2 = random.Next(1, 7);
+            int num3 = random.Next(1, 7);
+
+
+
+            Console.WriteLine(num1);
+            Console.WriteLine(num2);
+            Console.WriteLine(num3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}

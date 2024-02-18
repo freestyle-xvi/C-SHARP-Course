fe6abfd98220bbf4ee474de0ceecb6a32c5edc00
freestyle-XVI <90510503+freestyle-xvi@users.noreply.github.com>
2024-02-18 14:07:23 +0200

    Add files via upload

---
+++ #18 Project/project-xviii/project-xviii/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace project_xviii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //arrays = a variable that can store multiple values. fixed size
+
+            //String[] cars = { "GTR", "RX7", "AE86" };
+
+            String[] cars = new string[3];
+
+            cars[0] = "R33";
+            cars[1] = "RX7";
+            cars[2] = "AE86";
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ #19 Project/project-xix/project-xix/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace project_xix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //foreach loop = a simpler way to iterate over an array, but it's less fexible
+
+            String[] cars = { "GTR", "RX7", "AE86" };
+            foreach(String car in cars)
+            {
+                Console.WriteLine(car);
+            }
+            
+            
+            
+            
+            
+            Console.ReadKey();
+        }
+    }
+}

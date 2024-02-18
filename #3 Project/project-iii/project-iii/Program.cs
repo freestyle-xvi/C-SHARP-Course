48059fa66ac6e855eb02a799fa272deb308ba0c7
freestyle-XVI <90510503+freestyle-xvi@users.noreply.github.com>
2024-02-18 13:20:04 +0200

    Add files via upload

---
+++ #3 Project/project-iii/project-iii/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace project_iii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //user inputs
+            Console.WriteLine("What's your name?");//ask's user to input name
+            string name = Console.ReadLine();//user inputs name
+
+            Console.WriteLine("\nWhat's your age?");//ask's user to input age
+            int age = Convert.ToInt32(Console.ReadLine());  //user inputs age //also converts the input to an int   
+
+            Console.WriteLine("\n" + "hello " + name );
+            Console.WriteLine("Your age is " + age + " years old");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}

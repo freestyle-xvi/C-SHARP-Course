f86d5da57add2b293e4655e1b71fa7ac7f28a8e5
freestyle-XVI <90510503+freestyle-xvi@users.noreply.github.com>
2024-02-18 14:02:51 +0200

    Add files via upload

---
+++ #12 Project/project-xii/project-xii/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace project_xii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while loops = repeats code when conditions are true
+
+            
+            string name = "";
+
+            while(name == "")
+            {
+                Console.Write("Enter your name: ");
+                name = Console.ReadLine();
+            }
+
+
+
+
+
+            Console.WriteLine("hello " + name);
+
+            
+            
+            
+            
+            
+            
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ #13 Project/project-xiii/project-xiii/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace project_xiii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for loops = repeats code a finite amount of times
+
+            /*for (int i = 1; i <= 21; i +=2) //list numbers till 100, will stop by 21// "i +=2" only shows every 2nd index amount
+                {
+                Console.WriteLine(i);
+            }
+            */
+
+            for(int i =10; i > 0; i--)  //counts down numbers from 10   
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("THE END!!!");
+
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}

50bb799fbcc5bd92ad0db361a42fb78338d16c98
freestyle-XVI <90510503+freestyle-xvi@users.noreply.github.com>
2024-02-18 13:02:45 +0200

    Add files via upload

---
+++ #1 Project/project-i/project-i/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace project_i
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("DnA");
+            Console.WriteLine("freestyle");
+            Console.WriteLine("XVI");
+            Console.Beep();
+            Console.Beep();
+            Console.Beep();
+            //doing c# while waiting for acces to play mw again s(pain)
+            /*lmfao im so bored without mw though ngl
+             * like what do i even do*/
+            Console.WriteLine("\tfreestyle.XVI");
+            Console.WriteLine("\n[DnA]\n[Clan]\n[Official]");
+            Console.ReadKey();
+        }
+    }
+}
